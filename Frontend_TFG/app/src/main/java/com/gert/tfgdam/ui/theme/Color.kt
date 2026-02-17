ccbf1b8bf52987147c9614e401c581288ccad11b
Gertswito <gertjustgonzalezbreto@gmail.com>
2026-02-17 22:39:00 +0100

    main - Cambiado icono de la App y el primary color a uno un poquitín más oscuro

--- Frontend_TFG/app/src/main/java/com/gert/tfgdam/ui/theme/Color.kt
+++ Frontend_TFG/app/src/main/java/com/gert/tfgdam/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.gert.tfgdam.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val VerdeOscurillo = Color(0xFF3D6D5C)
+val VerdeOscurillo = Color(0xFF31574A)
 
 val Dorado = Color(0xFFCBB26A)
 
@@ -12,10 +12,10 @@ val Blanco = Color(0xFFFFFFFF)
 
 val CasiBlack = Color(0xFF2B2B2B)
 
-val CasiWhite = Color(0xFFEEEEEE)
+val CasiWhite = Color(0xFFE7E7E7)
 
 val NegroClarito = Color(0xFF1E1E1E)
 
-val BlancoOscurito = Color(0xFFD5D5D5)
+val BlancoOscurito = Color(0xFFDCDCDC)
 
 val ErrorRojo = Color(0xFFFF304D)
